022e1ea8bcbc6266dc9bd297fbecf73948caeed7
Moones <moones@email.cz>
2014-06-25 01:23:48 +0200

    Checking if target has escape ability/item
    And if so then eat'em.

--- Scripts/Pudges_Butchery.lua
+++ Scripts/Pudges_Butchery.lua
@@ -14,12 +14,12 @@ targetHandle = nil
 
 local xx,yy = 10,client.screenSize.y/25.714
 local myFont = drawMgr:CreateFont("Pudge","Tahoma",14,550)
-local statusText = drawMgr:CreateText(xx,yy,-1,"Pudge Script: ON",myFont);
-local targetText = drawMgr:CreateText(xx,yy+15,-1,"",myFont);
+local statusText = drawMgr:CreateText(-30,-20,-1,"Hook'em!",myFont);
+local targetText = drawMgr:CreateText(-100,-5,-1,"",myFont);
 local reg = nil
 local active = true
 DmgD = {225,375,525}
-
+DmgR = {35,60,85,110}
 
 targetText.visible = false
 
@@ -29,65 +29,21 @@ function Key(msg,code)
 
 	if not active then
 		active = true
-		statusText.text = "Pudge Script: ON"
+		statusText.text = "Hook'em!"
 		return true
 	else
 		active = nil
-		statusText.text = "Pudge Script: OFF"
+		statusText.text = "   OFF!"
 		return true
 	end
 
 end
 
---[[ function Autohook(msg,code)
-	if msg ~= KEY_UP or code ~= hookkey or client.chat then	return end
-	
-	local Spell = me:GetAbility(ability)
-	icon.textureId = drawMgr:GetTextureId("NyanUI/spellicons/"..Spell.name)
-	if Spell.level > 0 then
-		local Dmg = SmartGetDmg(COMPLEX,Spell.level,me,damage,adamage,id)
-		local DmgT = GetDmgType(Spell,tdamage)
-		local CastPoint = Spell:GetCastPoint(Spell.level)+client.latency/1000
-		if me.alive and not me:IsChanneling() then
-			local enemies = entityList:GetEntities({type=LuaEntity.TYPE_HERO,team = me:GetEnemyTeam(),illusion=false})			
-			for i,v in ipairs(enemies) do
-				if v.healthbarOffset ~= -1 then
-					if not hero[v.handle] then
-						hero[v.handle] = drawMgr:CreateText(20,0-45, 0xFFFFFF99, "",F14) hero[v.handle].visible = false hero[v.handle].entity = v hero[v.handle].entityPosition = Vector(0,0,v.healthbarOffset)
-					end
-					if v.visible and v.alive and v.health > 1 then
-						hero[v.handle].visible = draw
-						local DmgS = math.floor(v:DamageTaken(Dmg,DmgT,me))
-						local DmgF = math.floor(v.health - v:DamageTaken(DmgS,DmgT,me) + CastPoint*v.healthRegen)
-						hero[v.handle].text = " "..DmgF
-						if activ then
-							if DmgF < 0 and KSCanDie(v,me) and (not me:IsMagicDmgImmune() and NotDieFromSpell(Spell,v,me) and not v:DoesHaveModifier("modifier_nyx_assassin_spiked_carapace") and NotDieFromBM(v,me,DmgS)) then
-								local move = v.movespeed local pos = v.position	local distance = GetDistance2D(v,me)
-								if v.activity == LuaEntityNPC.ACTIVITY_MOVE and v:CanMove() then																		
-									local range = Vector(pos.x + move * (distance/(project * math.sqrt(1 - math.pow(move/project,2))) + cast) * math.cos(v.rotR), pos.y + move * (distance/(project * math.sqrt(1 - math.pow(move/project,2))) + cast) * math.sin(v.rotR), pos.z)
-									if GetDistance2D(me,range) < Spell.castRange + 25 then									
-										me:SafeCastAbility(Spell,range)	break
-									end
-								elseif distance < Spell.castRange + 25 then
-									me:SafeCastAbility(Spell,Vector(pos.x + move * 0.05 * math.cos(v.rotR), pos.y + move* 0.05 * math.sin(v.rotR), pos.z)) break
-								end
-							end
-						end
-					else
-						hero[v.handle].visible = false
-					end
-				end
-			end
-		end
-	end	
-end ]]--
-
 function Tick( tick )
 	if tick < sleeptick or not IsIngame() or client.console or client.paused then
 		return
 	end
-
-	sleeptick = tick + 100
+	sleeptick = tick + 50 + client.latency
 	
 	local me = entityList:GetMyHero()
 	local player = entityList:GetMyPlayer()
@@ -103,7 +59,7 @@ function Tick( tick )
 	if not target or not target.visible or not target.alive or not me.alive or not active or target:IsUnitState(LuaEntityNPC.STATE_MAGIC_IMMUNE) then
 		targetHandle = nil
 		targetText.visible = false
-		statusText.text = "Pudge Script: ON"
+		statusText.text = "Hook'em!"
 		active = true
 		script:UnregisterEvent(Tick)
 		return
@@ -122,7 +78,7 @@ function Tick( tick )
 	if distance > minRange then
 		targetHandle = nil
 		targetText.visible = false
-		statusText.text = "Pudge Script: ON"
+		statusText.text = "Hook'em!"
 		active = true
 		script:UnregisterEvent(Tick)
 		return
@@ -140,17 +96,15 @@ function Tick( tick )
 	end
 	
 	if urn and urn.charges ~= 0 and urn.state == -1 and not target:DoesHaveModifier("modifier_item_urn_damage") and aga and R.level > 0 and R.cd > 0 and me:IsChanneling() == false then
-		if target.health > (DmgD[R.level]+(3*me.strengthTotal) * (1 - target.magicDmgResist)) then
+		if target.health > (DmgD[R.level]+(3*me.strengthTotal) * (1 - target.magicDmgResist)) or CanEscape(target) == true then
 			me:SafeCastItem(urn.name,target)
-		elseif target.health < (DmgD[R.level]+(3*me.strengthTotal) * (1 - target.magicDmgResist)) and R.state ~= LuaEntityAbility.STATE_READY then
+		elseif target.health < (DmgD[R.level]+(3*me.strengthTotal) * (1 - target.magicDmgResist)) and R.state ~= LuaEntityAbility.STATE_READY or CanEscape(target) == true then
 			me:SafeCastItem(urn.name,target)
 		end
 	end
-	
-	if R.level > 0 and R.state == LuaEntityAbility.STATE_READY and (target.health*(target.dmgResist+1)) > ((me.dmgMin + me.dmgBonus)*3) then 
+	if R.level > 0 and R.state == LuaEntityAbility.STATE_READY and (target.health*(target.dmgResist+1)) > ((me.dmgMin + me.dmgBonus)*3) or CanEscape(target) == true then 
 		me:SafeCastSpell(R.name,target)
 	end
-	
 	if R.cd > 0 and me:IsChanneling() == false then
 		if distance > 150 and (target.health*(target.dmgResist+1)) > ((me.dmgMin + me.dmgBonus)) then
 			me:Move(target.position)
@@ -170,6 +124,13 @@ function target(tick)
 	if not me then
 		return
 	end
+	local offset = me.healthbarOffset
+	
+	statusText.entity = me
+	statusText.entityPosition = Vector(0,0,offset)
+	targetText.entity = me
+	targetText.entityPosition = Vector(0,0,offset)
+	
 	
 	if not reg then
 		script:RegisterEvent(EVENT_KEY,Key)
@@ -190,13 +151,26 @@ function target(tick)
 				if v:DoesHaveModifier("modifier_pudge_meat_hook") then
 					targetHandle = v.handle
 					targetText.visible = true
-					targetText.text = "Killing " .. client:Localize(v.name)
+					targetText.text = "Eating " .. client:Localize(v.name) .. ". Press " .. string.char(key) .. " to cancel."
 					script:RegisterEvent(EVENT_TICK,Tick)
 				end
 			end
 		end
 	end
 end
+
+function CanEscape(who)
+	local me = entityList:GetMyHero()
+	local abilities = me.abilities
+	local W = abilities[2]
+	wID = who.classId
+	if (who.health > DmgR[W.level]) and (wID == CDOTA_Unit_Hero_Anti_Mage or wID == CDOTA_Unit_Hero_Mirana or wID == CDOTA_Unit_Hero_Queen_of_Pain or wID == CDOTA_Unit_Hero_Windrunner or wID == CDOTA_Unit_Hero_Shredder or wID == CDOTA_Unit_Hero_Centaur or who:FindItem("item_force_staff")) then
+		return true
+	else
+		return	false
+	end
+end
+
 
 function GameClose()
 	if reg then
@@ -212,7 +186,7 @@ end
 function Load()
 	targetText.visible = false
 	statusText.visible = true
-	statusText.text = "Pudge Script: ON"
+	statusText.text = "Hook'em!"
 	active = true
 end
 
