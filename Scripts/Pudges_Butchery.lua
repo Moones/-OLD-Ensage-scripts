37938124cd1f4aa058a432ac7de9528f17c6919e
Moones <moones@email.cz>
2014-06-26 13:11:40 +0200

    Now requires SkillShot and VectorOp libs

--- Scripts/Pudges_Butchery.lua
+++ Scripts/Pudges_Butchery.lua
@@ -1,6 +1,8 @@
 require("libs.Utils")
 require("libs.ScriptConfig")
 require("libs.TargetFind")
+require("libs.SkillShot")
+require("libs.VectorOp")
 
 config = ScriptConfig.new()
 config:SetParameter("Hotkey", "F", config.TYPE_HOTKEY)
@@ -26,10 +28,6 @@ DmgD = {225,375,525}
 DmgR = {35,60,85,110}
 DmgR2 = {7,12,17,22}
 RangeH = {700,900,1100,1300}
-SkillShot = {}
-SkillShot.trackTable = {}
-SkillShot.lastTrackTick = 0
-SkillShot.currentTick = 0
 targetText.visible = false
 
 function HookKey(msg,code)
@@ -65,39 +63,22 @@ function Autohook(tick)
 	if not me then return end
 	local hook = me:GetAbility(1)
 	if hook.level > 0 and hookem then
-	hookem = nil
-		if me.alive then	
+		hookem = nil
+		if me.alive and victimHandle then		
 			local victim = entityList:GetEntity(victimHandle)		
 			if victim and victim.visible and victim.alive and victim.health > 1 then
 				if not victim:DoesHaveModifier("modifier_nyx_assassin_spiked_carapace") then
-					local move = victim.movespeed 
-					local pos = victim.position	
-					local mepos = me.position
-					local distance = GetDistance2D(victim,me) 
-					local project = 1600 
-					local cast = 300
+					local speed = 1600 
+					local castPoint = 300
+					local aoe = 100
 					local delay = client.latency
-					local delay = cast + delay
-					if not victim:CanMove() then
-						local prediction = Vector(pos.x, pos.y, 0)
-						me:SafeCastAbility(hook, prediction)
-					else
-						local delay1 = (delay + (distance*1000/project))
-						local stage1 = SkillShot.PredictedXYZ(victim,delay1)
-						if stage1 then
-							local distance1 = math.sqrt(math.pow(mepos.x-stage1.x,2)+math.pow(mepos.y-stage1.y,2))
-							local delay2 = delay + (distance1*1000/project)
-							local stage2 = SkillShot.PredictedXYZ(victim,delay2)
-							local i = 1
-							while math.floor(distance1) ~= math.floor(math.sqrt(math.pow(mepos.x-stage1.x,2)+math.pow(mepos.y-stage1.y,2))) do
-							stage1 = stage2
-							distance1 = math.sqrt(math.pow(mepos.x-stage1.x,2)+math.pow(mepos.y-stage1.y,2))
-							delay2 = delay + (distance1*1000/project)
-							stage2 = SkillShot.PredictedXYZ(victim,delay2)
-							i = i + 1
-							end
-							me:SafeCastAbility(hook,Vector(stage2.x,stage2.y,stage2.z))
+					local xyz = SkillShot.SkillShotXYZ(me,victim,castPoint + delay,speed)
+					if xyz then
+						if me:GetDistance2D(xyz) > (RangeH[hook.level] + aoe) and RangeH[hook.level] ~= 0 then
+							xyz = (xyz - me.position) * (RangeH[hook.level] - 100) / me:GetDistance2D(xyz) + me.position
 						end
+					me:SafeCastAbility(hook, xyz)
+					Sleep(250)
 					end
 				end
 			end
@@ -181,43 +162,12 @@ function Tick( tick )
 	end
 end
 
-function SkillShot.__Track()
-	local all = entityList:GetEntities({type = TYPE_HERO})
-	for i,v in ipairs(all) do
-		if SkillShot.trackTable[v.handle] == nil and v.alive and v.visible then
-			SkillShot.trackTable[v.handle] = {nil,nil,nil,v,nil}
-		elseif SkillShot.trackTable[v.handle] ~= nil and (not v.alive or not v.visible) then
-			SkillShot.trackTable[v.handle] = nil
-		elseif SkillShot.trackTable[v.handle] then
-			if SkillShot.trackTable[v.handle].last ~= nil then
-				SkillShot.trackTable[v.handle].speed = (v.position - SkillShot.trackTable[v.handle].last.pos)/(SkillShot.currentTick - SkillShot.trackTable[v.handle].last.tick)
-			end
-			SkillShot.trackTable[v.handle].last = {pos = v.position, tick = SkillShot.currentTick}
-		end
-	end
-end
-
-function SkillShot.PredictedXYZ(t,delay)
-	if t.CanMove and not t:CanMove() then
-		return Vector(t.x,t.y,0)
-	elseif SkillShot.trackTable[t.handle] and SkillShot.trackTable[t.handle].speed then
-		local v = t.position + SkillShot.trackTable[t.handle].speed * delay
-		return Vector(v.x,v.y,0)
-	end
-end
-
 function target(tick)
 	if not IsIngame() or client.console then return end
 	
 	local me = entityList:GetMyHero()
 	
 	if not me then return end
-	
-	SkillShot.currentTick = tick
-	if tick > SkillShot.lastTrackTick + 50 then
-		SkillShot.__Track()
-		SkillShot.lastTrackTick = tick 	
-	end
 	
 	local offset = me.healthbarOffset
 	
@@ -236,10 +186,10 @@ function target(tick)
 	if active then	
 		for i,v in ipairs(entityList:GetEntities({type=LuaEntity.TYPE_HERO,alive=true,illusion=false})) do
 			if v.team ~= me.team then
-				local victimm = targetFind:GetLowestEHP(1325)
+				local victimm = targetFind:GetLowestEHP(1350)
 				if victimm and victimm.visible and victimm.alive then
 				local distance = GetDistance2D(victimm,me) 
-					if distance < 1325 then
+					if distance < 1350 then
 						victimHandle = victimm.handle
 						statusText.text = "Hook: " .. client:Localize(victimm.name)
 					end
@@ -267,25 +217,26 @@ function AutoDeny(tick)
 	if not me or not me.alive then return end
 	
 	local rot = me:GetAbility(2)
-	
-	for i,v in ipairs(entityList:GetEntities({type=LuaEntity.TYPE_HERO,alive=true,illusion=false})) do
-		if v.team ~= me.team then
-			local distance = GetDistance2D(v,me)
-			local projectile = entityList:GetProjectiles({target=me, source=v})
-			if v:IsRanged() then
-				if projectile and distance <= (v.attackRange + 50) then
-					for k,z in ipairs(projectile) do
-						if me.health <= (DmgR2[rot.level]*3*(1 - me.magicDmgResist)) then
-							if rot.toggled == false then
-								me:SafeToggleSpell(rot.name)
+	if rot then
+		for i,v in ipairs(entityList:GetEntities({type=LuaEntity.TYPE_HERO,alive=true,illusion=false})) do
+			if v.team ~= me.team then
+				local distance = GetDistance2D(v,me)
+				local projectile = entityList:GetProjectiles({target=me, source=v})
+				if v:IsRanged() then
+					if projectile and distance <= (v.attackRange + 50) then
+						for k,z in ipairs(projectile) do
+							if me.health <= (DmgR2[rot.level]*3*(1 - me.magicDmgResist)) then
+								if rot.toggled == false then
+									me:SafeToggleSpell(rot.name)
+								end
 							end
 						end
 					end
-				end
-			else
-				if distance <= (v.attackRange + 50) and me.health <= (DmgR2[rot.level]*3*(1 - me.magicDmgResist)) then
-					if rot.toggled == false then
-						me:SafeToggleSpell(rot.name)
+				else
+					if distance <= (v.attackRange + 50) and me.health <= (DmgR2[rot.level]*3*(1 - me.magicDmgResist)) then
+						if rot.toggled == false then
+							me:SafeToggleSpell(rot.name)
+						end
 					end
 				end
 			end
@@ -295,8 +246,7 @@ end
 
 function CanEscape(who)
 	local me = entityList:GetMyHero()
-	local abilities = me.abilities
-	local W = abilities[2]
+	local W = me:GetAbility(2)
 	wID = who.classId
 	if (who.health > DmgR[W.level]) and (wID == CDOTA_Unit_Hero_Anti_Mage or wID == CDOTA_Unit_Hero_Mirana or wID == CDOTA_Unit_Hero_Queen_of_Pain or wID == CDOTA_Unit_Hero_Windrunner or wID == CDOTA_Unit_Hero_Shredder or wID == CDOTA_Unit_Hero_Centaur or wID == CDOTA_Unit_Hero_Earth_Spirit or wID == CDOTA_Unit_Hero_Ember_Spirit or wID == CDOTA_Unit_Hero_Storm_Spirit or who:FindItem("item_force_staff") or who.movespeed > 480) then
 		return true
