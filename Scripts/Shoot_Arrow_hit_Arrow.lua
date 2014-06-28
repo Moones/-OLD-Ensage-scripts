7175a5ef4a29ceb6d886bdb7d9648eba526eef75
Alois Pospíšil <moones@email.cz>
2014-06-29 01:07:50 +0200

    Update Shoot_Arrow_hit_Arrow.lua

--- Scripts/Shoot_Arrow_hit_Arrow.lua
+++ Scripts/Shoot_Arrow_hit_Arrow.lua
@@ -20,9 +20,10 @@ local active = true
 local shoot = nil
 RangeA = 3000
 
-function ArrowKey(msg,code)
-	if msg ~= KEY_UP or code ~= arrowkey or client.chat then return end
-	if active then
+function ArrowKey(msg,code)	
+	if msg ~= KEY_UP or client.chat then return end
+
+	if code == arrowkey and active then     
 		if not shoot then
 			shoot = true
 			return true
@@ -30,102 +31,87 @@ function ArrowKey(msg,code)
 			shoot = nil
 			return true
 		end
-	end
-end
-
-function Key(msg,code)	
-    if msg ~= KEY_UP or code ~= key or client.chat then	return end
-
-	if not active then
-		active = true
-		statusText.text = "Shoot Arrow hit Arrow!"
-		return true
-	else
-		active = nil
-		statusText.text = "   OFF!"
-		return true
-	end
-end
-
-function Arrow(tick)
-	if not IsIngame() or client.console or client.paused then return end
-	local me = entityList:GetMyHero()
-	if not me then return end
-	local arrow = me:GetAbility(2)
-	if arrow.level > 0 and shoot then
-		shoot = nil
-		if me.alive and victimHandle then		
-			local victim = entityList:GetEntity(victimHandle)		
-			if victim and victim.visible and victim.alive and victim.health > 1 then
-				if not victim:DoesHaveModifier("modifier_nyx_assassin_spiked_carapace") then
-					local speed = 857 
-					local distance = GetDistance2D(victim, me)
-					local castPoint = arrow:GetCastPoint(arrow.level)
-					local xyz = SkillShot.SkillShotXYZ(me,victim,speed,castPoint)
-					if xyz and distance < RangeA + 57.5 then	
-					me:SafeCastAbility(arrow, xyz)
-					Sleep(250)
-					end
-				end
-			end
+	elseif code == key and not active then
+		if not active then
+			active = true
+			statusText.text = "Shoot Arrow hit Arrow!"
+			return true
+		else
+			active = nil
+			statusText.text = "   OFF!"
+			return true
 		end
-	end	
+	end
 end
 
-function target(tick)
-	if not IsIngame() or client.console then return end
-	
+function Main(tick)
+	if not IsIngame() or client.console or not SleepCheck() then return end
+
+	Sleep(200)
+
 	local me = entityList:GetMyHero()
-	
+
 	if not me then return end
-	
-	local offset = me.healthbarOffset
-	
-	statusText.entity = me
-	statusText.entityPosition = Vector(0,0,offset)
-		
+
 	if me.classId ~= CDOTA_Unit_Hero_Mirana then
 		statusText.visible = false
 		script:Disable()
 		return
 	end
-	
-	if active then	
+
+	local offset = me.healthbarOffset
+
+	if not statusText.entity then
+		statusText.entity = me
+		statusText.entityPosition = Vector(0,0,offset)
+	end
+
+	local arrow = me:GetAbility(2)
+
+	if active then  
+
+		local victim = targetFind:GetLowestEHP(3057.5, magic)
+
+		if victim and GetDistance2D(victim,me) < 3057.5 then
+			statusText.text = "Shoot: " .. client:Localize(victim.name)
+			if shoot and arrow.level > 0 and me.alive then shoot = nil              
+				if not victim:DoesHaveModifier("modifier_nyx_assassin_spiked_carapace") then
+					local speed = 857 
+					local distance = GetDistance2D(victim, me)
+					local castPoint = arrow:GetCastPoint(arrow.level)
+					local xyz = SkillShot.SkillShotXYZ(me,victim,speed,castPoint)
+					if xyz and distance < 3000 + 57.5 then  
+					me:SafeCastAbility(arrow, xyz)
+					end
+				end
+			end 
+		else
+			statusText.text = "Shoot Arrow hit Arrow!"
+		end
+
 		for i,v in ipairs(entityList:GetEntities({type=LuaEntity.TYPE_HERO,alive=true,illusion=false})) do
-			if v.team ~= me.team then
-				local victimm = targetFind:GetLowestEHP(3057.5, magic)
-				if victimm and victimm.visible and victimm.alive then
-				local distance = GetDistance2D(victimm,me) 
-					if distance < 3057.5 then
-						victimHandle = victimm.handle
-						statusText.text = "Shoot: " .. client:Localize(victimm.name)
-					end
-				else
-					statusText.text = "Shoot Arrow hit Arrow!"
-				end
-				if v:DoesHaveModifier("modifier_shadow_demon_disruption") then
-				local distime = v:FindModifier("modifier_shadow_demon_disruption").remainingTime
-				local arrow = me:GetAbility(2)
+
+			if v.team ~= me.team and GetDistance2D(v,me) < 3057.5 then
+
+				local disruption = v:DoesHaveModifier("modifier_shadow_demon_disruption")
+				local astral = v:DoesHaveModifier("modifier_obsidian_destroyer_astral_imprisonment_prison")
+				local eul = v:DoesHaveModifier("modifier_eul_cyclone")
+
+				if disruption then              
 					if GetDistance2D(v,me) <= 2200 then
-						if (distime * 857) == GetDistance2D(v,me)+115 or ((distime * 857) < GetDistance2D(v,me)+115 and (distime * 857)+25 > GetDistance2D(v,me)) then
+						if (disruption.remainingTime * 857) == GetDistance2D(v,me)+115 or ((disruption.remainingTime * 857) < GetDistance2D(v,me)+115 and (disruption.remainingTime * 857)+25 > GetDistance2D(v,me)) then
+							me:SafeCastAbility(arrow, v.position)
+						end
+					end             
+				elseif astral then
+					if GetDistance2D(v,me) <= (astral.remainingTime*857+57.5) then
+						if (astral.remainingTime * 857) == GetDistance2D(v,me)+140 or ((astral.remainingTime * 857) < GetDistance2D(v,me)+140 and (astral.remainingTime * 857)+25 > GetDistance2D(v,me)) then
 							me:SafeCastAbility(arrow, v.position)
 						end
 					end
-				end
-				if v:DoesHaveModifier("modifier_obsidian_destroyer_astral_imprisonment_prison") then
-				local odtime = v:FindModifier("modifier_obsidian_destroyer_astral_imprisonment_prison").remainingTime
-				local arrow = me:GetAbility(2)
-					if GetDistance2D(v,me) <= (odtime*857+57.5) then
-						if (odtime * 857) == GetDistance2D(v,me)+140 or ((odtime * 857) < GetDistance2D(v,me)+140 and (odtime * 857)+25 > GetDistance2D(v,me)) then
-							me:SafeCastAbility(arrow, v.position)
-						end
-					end
-				end
-				if v:DoesHaveModifier("modifier_eul_cyclone") then
-				local cyctime = v:FindModifier("modifier_eul_cyclone").remainingTime
-				local arrow = me:GetAbility(2)
-					if GetDistance2D(v,me) <= (cyctime*857+57.5) then
-						if (cyctime * 857) == GetDistance2D(v,me)+140 or ((cyctime * 857) < GetDistance2D(v,me)+140 and (cyctime * 857)+25 > GetDistance2D(v,me)) then
+				elseif eul then
+					if GetDistance2D(v,me) <= ( eul.remainingTime*857+57.5) then
+						if (eul.remainingTime * 857) == GetDistance2D(v,me)+140 or (( eul.remainingTime * 857) < GetDistance2D(v,me)+140 and ( eul.remainingTime * 857)+25 > GetDistance2D(v,me)) then
 							me:SafeCastAbility(arrow, v.position)
 						end
 					end
@@ -136,7 +122,5 @@ function target(tick)
 end
 
 
-script:RegisterEvent(EVENT_TICK,target)
-script:RegisterEvent(EVENT_TICK,Arrow)
-script:RegisterEvent(EVENT_KEY,Key)
+script:RegisterEvent(EVENT_TICK,Main)
 script:RegisterEvent(EVENT_KEY,ArrowKey)
