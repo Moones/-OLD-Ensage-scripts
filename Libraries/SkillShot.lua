259b6636dacd4b24080a8ec4ec5f42fa6c396b16
Moones <moones@email.cz>
2014-06-28 03:34:17 +0200

    Update SkillShot.lua

--- Libraries/SkillShot.lua
+++ Libraries/SkillShot.lua
@@ -11,18 +11,20 @@ require("libs.VectorOp")
  0 1 1 0 0 0 0 1    
  0 1 1 1 1 0 0 0    
 
-			SkillShot Library v1.3
+			SkillShot Library v1.4
 
 		Save as SkillShot.lua into Ensage\Scripts\libs.
 
 		Functions:
 			SkillShot.InFront(target,distance): Returns the Vector of the position in front of the target for specified distance
 			SkillShot.PredictedXYZ(target,delay): Returns the Vector of the target's predicted location after specified milisecond
-			SkillShot.SkillShotXYZ(source,target,delay,speed): Returns the Vector of the target's predicted location for a  Souce is the caster,speed is the speed of the projectile and delay is the casting time
+			SkillShot.SkillShotXYZ(source,target,speed,castpoint): Returns the Vector of the target's predicted location for a Source is the caster,speed is the speed of the projectile and castpoint is the casting time
 			(Currently not working)SkillShot.BlockableSkillShotXYZ(source,target,delay,speed,aoe,team): Same as SkillShotXYZ, but this time it returns nil if skillshot can be blocked by a unit. AoE is aoe of the spell. Team is true if allies can block, false otherwise.
 
 
 		Changelog:
+			v1.4:
+			 - Improved prediction
 			v1.3:
 			 - Reworked for new version
 			 
@@ -50,31 +52,14 @@ SkillShot.currentTick = 0
 function SkillShot.__TrackTick(tick)
 	SkillShot.currentTick = tick
 	if tick > SkillShot.lastTrackTick + 50 then
-		SkillShot.__Track()
 		SkillShot.lastTrackTick = tick 	
-	end
-end
-
-function SkillShot.__Track()
-	local all = entityList:GetEntities({type = TYPE_HERO})
-	for i,v in ipairs(all) do
-		if SkillShot.trackTable[v.handle] == nil and v.alive and v.visible then
-			SkillShot.trackTable[v.handle] = {nil,nil,nil,v,nil}
-		elseif SkillShot.trackTable[v.handle] ~= nil and (not v.alive or not v.visible) then
-			SkillShot.trackTable[v.handle] = nil
-		elseif SkillShot.trackTable[v.handle] then
-			if SkillShot.trackTable[v.handle].last ~= nil then
-				SkillShot.trackTable[v.handle].speed = (v.position - SkillShot.trackTable[v.handle].last.pos)/(SkillShot.currentTick - SkillShot.trackTable[v.handle].last.tick)
-			end
-			SkillShot.trackTable[v.handle].last = {pos = v.position, tick = SkillShot.currentTick}
-		end
 	end
 end
 
 function SkillShot.InFront(t,distance)
 	local alpha = t.rotR
 	if alpha then
-		local v = t.position + vectorOp:UnitVectorFromXYAngle(alpha) * distance
+		local v = t.position + VectorOp.UnitVectorFromXYAngle(alpha) * distance
 		return Vector(v.x,v.y,0)
 	end
 end
@@ -82,34 +67,40 @@ end
 function SkillShot.PredictedXYZ(t,delay)
 	if not t:CanMove() then
 		return t.position
-	elseif SkillShot.trackTable[t.handle] and SkillShot.trackTable[t.handle].speed then
-		local v = t.position + SkillShot.trackTable[t.handle].speed * delay
-		return Vector(v.x,v.y,0)
+	else
+		tpos = t.position
+		local thandle = t.handle
+		local target = entityList:GetEntity(thandle)
+		local move = target.movespeed	
+		local v = Vector(tpos.x + move * delay * math.cos(t.rotR), tpos.y + move * delay * math.sin(t.rotR),tpos.z)
+		return v
 	end
 end
 
-function SkillShot.SkillShotXYZ(source,t,delay,speed,castpoint)
-	if not t:CanMove() then
+function SkillShot.SkillShotXYZ(source,t,speed,castpoint)
+	if t.activity ~= LuaEntityNPC.ACTIVITY_MOVE or not t:CanMove() then
 		return t.position
-	elseif source and t and delay and speed then
-		local delay1 = delay + (GetDistance2D(source,t)*1000/speed)
-		local stage1 = SkillShot.PredictedXYZ(t,delay1)
+	else
+		local thandle = t.handle
+		local target = entityList:GetEntity(thandle)
+		local move = target.movespeed		
+		local castpoint1 = (GetDistance2D(t,source)/(speed * math.sqrt(1 - math.pow(move/speed,2))) + castpoint)
+		local stage1 = SkillShot.PredictedXYZ(t,castpoint1)
 		if stage1 then
 			local sourcepos = source.position
-			local distance = math.sqrt(math.pow(sourcepos.x-stage1.x,2)+math.pow(sourcepos.y-stage1.y,2))
-			local delay2 = delay + (distance*1000/speed)
-			local stage2 = SkillShot.PredictedXYZ(t,delay2)
-			local i = 1
-			while (math.floor(distance) ~= math.floor(math.sqrt(math.pow(sourcepos.x-stage1.x,2)+math.pow(sourcepos.y-stage1.y,2)))) do
+			local distance = stage1:GetDistance2D(sourcepos)
+			local castpoint2 = (distance/(speed * math.sqrt(1 - math.pow(move/speed,2))) + castpoint)
+			local stage2 = SkillShot.PredictedXYZ(t,castpoint2)
+			while (math.floor(distance) ~= math.floor(stage2:GetDistance2D(sourcepos))) do
 				stage1 = stage2
-				distance = math.sqrt(math.pow(sourcepos.x-stage1.x,2)+math.pow(sourcepos.y-stage1.y,2))
-				delay2 = delay + (distance*1000/speed)
-				stage2 = SkillShot.PredictedXYZ(t,delay2)
-				i = i + 1
+				distance = stage1:GetDistance2D(sourcepos)
+				castpoint2 = (distance/(speed * math.sqrt(1 - math.pow(move/speed,2))) + castpoint)
+				stage2 = SkillShot.PredictedXYZ(t,castpoint2)
 			end
 			return Vector(stage2.x,stage2.y,stage2.z)
 		end
 	end
 end
+
 
 scriptEngine:RegisterLibEvent(EVENT_TICK,SkillShot.__TrackTick)
