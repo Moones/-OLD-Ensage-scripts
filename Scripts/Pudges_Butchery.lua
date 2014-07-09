005a2fedc359725a982a6a0245adfd7d46706a81
Alois Pospíšil <moones@email.cz>
2014-07-09 19:05:33 +0200

    Update Pudges_Butchery.lua

--- Scripts/Pudges_Butchery.lua
+++ Scripts/Pudges_Butchery.lua
@@ -87,12 +87,12 @@ function Main(tick)
 			victimText.visible = false
 		end
 		
-		local victim = targetFind:GetLowestEHP(1400, magic)
+		local victim = targetFind:GetLowestEHP(RangeH[hook.level] + aoe, magic)
 		if manualselection then
 			victim = targetFind:GetClosestToMouse(100)
 		end
 		
-		if victim and GetDistance2D(victim,me) <= 1400 then
+		if victim and GetDistance2D(victim,me) <= RangeH[hook.level] + aoe then
 			if not manualselection then
 				statusText.text = "Hook: " .. client:Localize(victim.name)
 			else
@@ -105,9 +105,10 @@ function Main(tick)
 				if not victim:DoesHaveModifier("modifier_nyx_assassin_spiked_carapace") then
 					local speed = 1600 
 					local delay = (300+client.latency)
+					local aoe = 100
 					local distance = GetDistance2D(victim, me)
-					local xyz = SkillShot.BlockableSkillShotXYZ(me,victim,speed,delay,100,true)
-					if xyz and distance <= 1400 then	
+					local xyz = SkillShot.BlockableSkillShotXYZ(me,victim,speed,delay,aoe,true)
+					if xyz and distance <= RangeH[hook.level] + aoe then	
 						me:SafeCastAbility(hook, xyz)
 						Sleep(250)
 					end
@@ -163,7 +164,7 @@ function Main(tick)
 					local speed = 1600 
 					local castPoint = (hook:GetCastPoint(hook.level) + client.latency)
 					local blindxyz = SkillShot.BlindSkillShotXYZ(me,v,speed,castPoint)
-					if blindxyz and blindxyz:GetDistance2D(me) <= 2000 then 
+					if blindxyz and blindxyz:GetDistance2D(me) <= RangeH[hook.level] + aoe then 
 						statusText.text = "Hook'em - BLIND!"
 						if hookem then hookem = nil
 							me:SafeCastAbility(hook, blindxyz)
