ea98e70b4dec28d55b23cf9194ee9919964fffe9
Alois Pospíšil <moones@email.cz>
2014-07-13 21:35:11 +0200

    Update 2.0
    Removed range_display effect, Added Icons which are visible on whole map, Added minimap icons.

--- Scripts/Blind_Prediction.lua
+++ Scripts/Blind_Prediction.lua
@@ -1,4 +1,6 @@
 require("libs.Utils")
+require("libs.Res")
+require("libs.DrawManager3D")
 
 Prediction = {}
 
@@ -37,21 +39,34 @@ function Prediction.BlindPrediction(t)
 	elseif Prediction.BlindPredictionTable[t.handle] and Prediction.trackTable[t.handle] and Prediction.trackTable[t.handle].last then
 		Prediction.BlindPredictionTable[t.handle].move = Prediction.trackTable[t.handle].last.move
 		local pos = Prediction.trackTable[t.handle].last.pos local distance = Prediction.BlindPredictionTable[t.handle].move/(Prediction.BlindPredictionTable[t.handle].move/50) local cast = 0 local rotR = Prediction.BlindPredictionTable[t.handle].rotR
+		if not Prediction.BlindPredictionTable[t.handle].icon then
+			Prediction.BlindPredictionTable[t.handle].icon = drawMgr:CreateRect(0,0,18,18,0x000000ff, drawMgr:GetTextureId("NyanUI/miniheroes/"..t.name:gsub("npc_dota_hero_",""))) Prediction.BlindPredictionTable[t.handle].icon.visible = false
+		end
+		if not Prediction.BlindPredictionTable[t.handle].iconmap then
+			Prediction.BlindPredictionTable[t.handle].iconmap = drawMgr3D:CreateRect(Vector(0,0,0), Vector(0,0,110), Vector2D(0,0), Vector2D(50,30), 0x000000ff, drawMgr:GetTextureId("NyanUI/heroes_horizontal/"..t.name:gsub("npc_dota_hero_",""))) Prediction.BlindPredictionTable[t.handle].iconmap.visible = false
+		end
 		if not t.visible then
-			if not Prediction.BlindPredictionTable[t.handle].eff then
+			Prediction.BlindPredictionTable[t.handle].icon.visible = true
+			Prediction.BlindPredictionTable[t.handle].iconmap.visible = true
+			if not Prediction.BlindPredictionTable[t.handle].range then
 				Prediction.BlindPredictionTable[t.handle].range = Vector(pos.x + Prediction.BlindPredictionTable[t.handle].move * (distance/(1600 * math.sqrt(1 - math.pow(Prediction.BlindPredictionTable[t.handle].move/1600,2))) + cast) * math.cos(t.rotR), pos.y + Prediction.BlindPredictionTable[t.handle].move * (distance/(1600 * math.sqrt(1 - math.pow(Prediction.BlindPredictionTable[t.handle].move/1600,2))) + cast) * math.sin(t.rotR), pos.z)	
-				Prediction.BlindPredictionTable[t.handle].eff = Effect(Prediction.BlindPredictionTable[t.handle].range,  "range_display")
-				Prediction.BlindPredictionTable[t.handle].eff:SetVector(1, Vector(65,0,0) )
-				Prediction.BlindPredictionTable[t.handle].eff:SetVector(0, Prediction.BlindPredictionTable[t.handle].range)
+				Prediction.BlindPredictionTable[t.handle].minimap = MapToMinimap(Prediction.BlindPredictionTable[t.handle].range.x,Prediction.BlindPredictionTable[t.handle].range.y)
+				Prediction.BlindPredictionTable[t.handle].icon.x = Prediction.BlindPredictionTable[t.handle].minimap.x-20/2
+				Prediction.BlindPredictionTable[t.handle].icon.y = Prediction.BlindPredictionTable[t.handle].minimap.y-20/2
+				Prediction.BlindPredictionTable[t.handle].iconmap.pos = Prediction.BlindPredictionTable[t.handle].range
 			else
-				if Prediction.BlindPredictionTable[t.handle].eff then
+				if Prediction.BlindPredictionTable[t.handle].range then
 					Prediction.BlindPredictionTable[t.handle].range = Vector(Prediction.BlindPredictionTable[t.handle].range.x + Prediction.BlindPredictionTable[t.handle].move * (distance/(1600 * math.sqrt(1 - math.pow(Prediction.BlindPredictionTable[t.handle].move/1600,2))) + cast) * math.cos(t.rotR), Prediction.BlindPredictionTable[t.handle].range.y + Prediction.BlindPredictionTable[t.handle].move * (distance/(1600 * math.sqrt(1 - math.pow(Prediction.BlindPredictionTable[t.handle].move/1600,2))) + cast) * math.sin(t.rotR),Prediction.BlindPredictionTable[t.handle].range.z)
-					Prediction.BlindPredictionTable[t.handle].eff:SetVector(0, Prediction.BlindPredictionTable[t.handle].range)
+					Prediction.BlindPredictionTable[t.handle].minimap = MapToMinimap(Prediction.BlindPredictionTable[t.handle].range.x,Prediction.BlindPredictionTable[t.handle].range.y)
+					Prediction.BlindPredictionTable[t.handle].icon.x = Prediction.BlindPredictionTable[t.handle].minimap.x-20/2
+					Prediction.BlindPredictionTable[t.handle].icon.y = Prediction.BlindPredictionTable[t.handle].minimap.y-20/2
+					Prediction.BlindPredictionTable[t.handle].iconmap.pos = Prediction.BlindPredictionTable[t.handle].range
 				end
 			end
 		else
-			Prediction.BlindPredictionTable[t.handle].eff = nil
-			collectgarbage("collect")
+			Prediction.BlindPredictionTable[t.handle].icon.visible = false
+			Prediction.BlindPredictionTable[t.handle].iconmap.visible = false
+			Prediction.BlindPredictionTable[t.handle].range = nil
 		end
 	end
 end
