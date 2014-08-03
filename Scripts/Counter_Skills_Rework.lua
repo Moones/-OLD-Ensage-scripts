ef443e4604c6b72dd2afc6dcf909dc0d3aad28ee
Alois Pospíšil <moones@email.cz>
2014-08-04 00:45:13 +0200

    Update Counter_Skills_Rework.lua

--- Scripts/Counter_Skills_Rework.lua
+++ Scripts/Counter_Skills_Rework.lua
@@ -23,6 +23,20 @@ function Tick( tick )
 						end
 					end
 				end
+			if v.name == "npc_dota_hero_axe" then
+				if v:GetAbility(4) and v:GetAbility(4).level > 0 and v:GetAbility(4).abilityPhase then
+					if GetDistance2D(v,me) < 150 then
+						turntime = (math.max(math.abs(FindAngleR(v) - math.rad(FindAngleBetween(v, me))) - 0.20, 0))
+						if turntime == 0 then
+							UseShadowBlade()
+							UseBlinkDagger()
+							UseEulScepterTarget()
+							PuckW()
+							UseSheepStickTarget()
+							UseOrchidtarget()
+						end
+					end
+				end
 			elseif v.name == "npc_dota_hero_puck" then
 				if v:GetAbility(2) and v:GetAbility(2).level > 0 and v:GetAbility(2).abilityPhase  then
 					if GetDistance2D(v,me) < 400 then
