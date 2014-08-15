b6a7ee5d4fc762d206a850f7f1fbf7f0ee3d0683
Alois Pospíšil <moones@email.cz>
2014-08-15 12:38:12 +0200

    Update 1.7b:
    Added immediate Auto Rot activation after hook, so you prevent Rubcik from stealing your Hook.
    Increased speed of combo.
    Pudge will now Automaticly HoldPosition after a succesfull hook to ensure instant Dismember after enemy lands.

--- Scripts/Pudges_Butchery.lua
+++ Scripts/Pudges_Butchery.lua
@@ -13,7 +13,7 @@ config:Load()
 local togglekey = config.Hotkey local hookkey = config.Hookkey local manualtogglekey = config.ManualtoggleKey
 
 sleeptick = 0 sleeptickk = 0
-targetHandle = nil local hookem = nil local manualselection = false local active = true local victim = nil local blindxyz = nil
+targetHandle = nil local manualselection = false local active = true local victim = nil local blindxyz = nil local rottoggled = false local count = 0
 
 local myFont = drawMgr:CreateFont("Pudge","Tahoma",14,550)
 local statusText = drawMgr:CreateText(-40,-20,-1,"Hook'em!",myFont);
@@ -24,107 +24,72 @@ local DmgD = {225,375,525} local DmgR = {35,60,85,110} local DmgR2 = {7,12,17,22
 targetText.visible = false victimText.visible = false
 
 function Key(msg,code)	
-    if msg ~= KEY_UP or client.chat then return end
-	if code == togglekey then
-		if not active then
-			active = true
-			statusText.text = "  Hook'em!"
-		else
-			active = nil
-			statusText.text = "   OFF!"
-			victimText.visible = false
-		end
-	elseif code == hookkey then
-		if active and victim and victim.visible then
-			hookem = not hookem
-		end
-	elseif code == manualtogglekey then
-		if active then
-			if not manualselection then
-				manualselection = true
-				statusText.text = "Hook'em - Manual!"
-			else
-				manualselection = nil
+    if client.chat then return end
+	if msg == KEY_UP then
+		if code == togglekey then
+			if not active then
+				active = true
 				statusText.text = "  Hook'em!"
+			else
+				active = nil
+				statusText.text = "   OFF!"
+				victimText.visible = false
+			end
+		elseif code == manualtogglekey then
+			if active then
+				if not manualselection then
+					manualselection = true
+					statusText.text = "Hook'em - Manual!"
+				else
+					manualselection = nil
+					statusText.text = "  Hook'em!"
+				end
+			end
+		end	
+	elseif msg == RBUTTON_UP then
+		if targetHandle then
+			if count == 0 then
+				count = 1
+			elseif count == 1 then
+				count = 2
 			end
 		end
-	end	
+	end
 end
 
 function Main(tick)
-	if not IsIngame() or client.console or not SleepCheck() then return end
-	
+	if not PlayingGame() or client.console then return end	
 	local me = entityList:GetMyHero()
-	
 	if not me then return end
-	
 	if me.classId ~= CDOTA_Unit_Hero_Pudge then
 		targetText.visible = false
 		statusText.visible = false
 		script:Disable()
 		return
 	end
-	
 	local offset = me.healthbarOffset
-	
 	if not statusText.entity then
 		statusText.entity = me
 		statusText.entityPosition = Vector(0,0,offset)
 	end
-	
 	if not targetText.entity then
 		targetText.entity = me
 		targetText.entityPosition = Vector(0,0,offset)
-	end
+	end	
 	
 	local hook = me:GetAbility(1)
+	local rot = me:GetAbility(2)	
 	
 	if active then
-	
-		if hook.state == LuaEntityAbility.STATE_READY then
-			victimText.visible = true
+		if hook.abilityPhase then
+			if rot.level > 0 and (rot.toggled == false and not rot.abilityPhase and not me:DoesHaveModifier("modifier_pudge_rot")) and rottoggled == false and SleepCheck("rot") then
+				rottoggled = true
+				me:SafeToggleSpell(rot.name)
+				Sleep(250 + client.latency, "rot")
+			end
 		else
-			victimText.visible = false
+			rottoggled = false
 		end
-		
-		if hook.level > 0 then
-			victim = targetFind:GetLowestEHP(RangeH[hook.level] + 100, magic)
-			if manualselection then
-				victim = targetFind:GetClosestToMouse(100)
-				statusText.text = "Hook'em - Manual!"
-			end
-			if victim then
-				local distance = GetDistance2D(victim, me)
-				if distance <= RangeH[hook.level] + 100 and victim.visible then
-					statusText.text = "Hook: " .. client:Localize(victim.name)
-					victimText.text = "  Hook'em!"
-					victimText.entity = entityList:GetEntity(victim.handle)
-					victimText.entityPosition = Vector(0,0,entityList:GetEntity(victim.handle).healthbarOffset)
-					if hookem and me.alive then hookem = nil
-						if not victim:DoesHaveModifier("modifier_nyx_assassin_spiked_carapace") then
-							local speed = 1600 
-							local delay = (300+client.latency)
-							local xyz = SkillShot.BlockableSkillShotXYZ(me,victim,speed,delay,100,true)
-							if xyz then	
-								me:SafeCastAbility(hook, xyz)
-								Sleep(250)
-							end
-						end
-					end
-				end
-			else
-				if not manualselection then
-					statusText.text = "  Hook'em!"
-					victimText.visible = false
-				else
-					statusText.text = "Hook'em - Manual!"
-					victimText.visible = false
-				end
-			end
-		end
-		
-		local rot = me:GetAbility(2)
-		
 		for i,v in ipairs(entityList:GetEntities({type=LuaEntity.TYPE_HERO,alive=true})) do	
 			if v.team ~= me.team and not v:IsIllusion() then
 				if v:DoesHaveModifier("modifier_pudge_meat_hook") then
@@ -135,7 +100,7 @@ function Main(tick)
 					if targetHandle == v.handle then
 						victimText.visible = false
 					end
-				end	
+				end
 				if rot.level > 0 then
 					
 					local distance = GetDistance2D(v,me)
@@ -169,11 +134,51 @@ function Main(tick)
 					blindxyz = SkillShot.BlindSkillShotXYZ(me,blindvictim,speed,castPoint)
 					if blindxyz and blindxyz:GetDistance2D(me) <= RangeH[hook.level] + 100 then 
 						statusText.text = "Hook'em - BLIND!"
-						if IsKeyDown(hookkey) then
+						if IsKeyDown(hookkey) and SleepCheck() and not client.chat then
 							me:SafeCastAbility(hook, blindxyz)
-							Sleep(250)
+							Sleep(100+client.latency)
 						end
 					end
+				end
+			end
+		end
+		if hook.state == LuaEntityAbility.STATE_READY then
+			victimText.visible = true
+		else
+			victimText.visible = false
+		end
+		if hook.level > 0 then
+			victim = targetFind:GetLowestEHP(RangeH[hook.level] + 100, magic)
+			if manualselection then
+				victim = targetFind:GetClosestToMouse(100)
+				statusText.text = "Hook'em - Manual!"
+			end
+			if victim then
+				local distance = GetDistance2D(victim, me)
+				if distance <= RangeH[hook.level] + 100 and victim.visible then
+					statusText.text = "Hook: " .. client:Localize(victim.name)
+					victimText.text = "  Hook'em!"
+					victimText.entity = entityList:GetEntity(victim.handle)
+					victimText.entityPosition = Vector(0,0,entityList:GetEntity(victim.handle).healthbarOffset)
+					if IsKeyDown(hookkey) and me.alive and not client.chat then
+						if not victim:DoesHaveModifier("modifier_nyx_assassin_spiked_carapace") then
+							local speed = 1600 
+							local delay = (300+client.latency)
+							local xyz = SkillShot.BlockableSkillShotXYZ(me,victim,speed,delay,100,true)
+							if xyz and SleepCheck() then	
+								me:SafeCastAbility(hook, xyz)
+								Sleep(100+client.latency)
+							end
+						end
+					end
+				end
+			else
+				if not manualselection then
+					statusText.text = "  Hook'em!"
+					victimText.visible = false
+				else
+					statusText.text = "Hook'em - Manual!"
+					victimText.visible = false
 				end
 			end
 		end
@@ -196,7 +201,7 @@ function Combo(tick)
 	local W = abilities[2]
 	local R = abilities[4]
 	
-	if not target or not target.visible or not target.alive or not me.alive or not active or target:IsUnitState(LuaEntityNPC.STATE_MAGIC_IMMUNE) or distance > minRange then
+	if not target or not target.visible or not target.alive or not me.alive or not active or target:IsUnitState(LuaEntityNPC.STATE_MAGIC_IMMUNE) or (distance > minRange and target and not target:DoesHaveModifier("modifier_pudge_meat_hook")) or count == 2 then
 		targetHandle = nil
 		targetText.visible = false
 		if not manualselection then
@@ -204,23 +209,26 @@ function Combo(tick)
 		else
 			statusText.text = "Hook'em - Manual!"
 		end
-		if W.toggled == true then
+		if W.toggled == true and me:DoesHaveModifier("modifier_pudge_rot") then
 			me:SafeToggleSpell(W.name)
 		end
 		active = true
+		count = 0
 		script:UnregisterEvent(Combo)
 		return
 	end
-	if W.level > 0 and W.toggled == false then
+	
+	if W.level > 0 and (W.toggled == false and not W.abilityPhase and not me:DoesHaveModifier("modifier_pudge_rot")) and rottoggled == false and SleepCheck("rot") then
 		if distance <= 250 then
 			me:SafeToggleSpell(W.name)
+			Sleep(250 + client.latency, "rot")
 		end
 	end
 		
 	local urn = me:FindItem("item_urn_of_shadows")
 	local aga = me:FindItem("item_ultimate_scepter")
 		
-	if urn and urn.charges > 0 and urn.state == -1 and not target:DoesHaveModifier("modifier_item_urn_damage")and not aga and R.level > 0 and R.cd < 27 and R.cd ~= 0 and not me:IsChanneling() then 
+	if urn and urn.charges > 0 and urn.state == -1 and not target:DoesHaveModifier("modifier_item_urn_damage")and not aga and ((R.level > 0 and not R.abilityPhase) or R.level == 0) and not me:IsChanneling() then 
 		if target.health > (DmgD[R.level] * (1 - target.magicDmgResist)) or CanEscape(target) then
 			me:SafeCastItem(urn.name,target)
 		elseif target.health < (DmgD[R.level] * (1 - target.magicDmgResist)) and R.state ~= LuaEntityAbility.STATE_READY or CanEscape(target) then
@@ -235,15 +243,19 @@ function Combo(tick)
 			me:SafeCastItem(urn.name,target)
 		end
 	end
-	if R.level > 0 and R.state == LuaEntityAbility.STATE_READY and (target.health*(target.dmgResist+1)) > ((me.dmgMin + me.dmgBonus)*3) or CanEscape(target) then 
-		me:SafeCastSpell(R.name,target)
-	end
-	if R.cd > 0 and not me:IsChanneling() then
-		if distance > 150 and (target.health*(target.dmgResist+1)) > ((me.dmgMin + me.dmgBonus)) then
-			me:Move(target.position)
-		else
-		me:Attack(target)
+	if not target:DoesHaveModifier("modifier_pudge_meat_hook") or GetDistance2D(me, target) < 1600*(0.35 + client.latency/1000) then
+		if R.level > 0 and R.state == LuaEntityAbility.STATE_READY and (target.health*(target.dmgResist+1)) > ((me.dmgMin + me.dmgBonus)*3) or CanEscape(target) then 
+			me:SafeCastSpell(R.name,target)
 		end
+		if R.cd > 0 and not me:IsChanneling() then
+			if distance > 150 and (target.health*(target.dmgResist+1)) > ((me.dmgMin + me.dmgBonus)) then
+				me:Move(target.position)
+			else
+			me:Attack(target)
+			end
+		end
+	else
+		entityList:GetMyPlayer():HoldPosition()
 	end
 end
 
@@ -252,7 +264,7 @@ function CanEscape(who)
 	local W = me:GetAbility(2)
 	wID = who.classId
 	if who and wID and W.level > 0 then
-		if (who.health > DmgR[W.level]) and (wID == CDOTA_Unit_Hero_Anti_Mage or wID == CDOTA_Unit_Hero_Mirana or wID == CDOTA_Unit_Hero_Queen_of_Pain or wID == CDOTA_Unit_Hero_Windrunner or wID == CDOTA_Unit_Hero_Shredder or wID == CDOTA_Unit_Hero_Centaur or wID == CDOTA_Unit_Hero_Earth_Spirit or wID == CDOTA_Unit_Hero_Ember_Spirit or wID == CDOTA_Unit_Hero_Storm_Spirit or who:FindItem("item_force_staff") or who.movespeed > 480) then
+		if (who.health > DmgR[W.level]) and (wID == CDOTA_Unit_Hero_Anti_Mage or wID == CDOTA_Unit_Hero_Mirana or wID == CDOTA_Unit_Hero_QueenOfPain or wID == CDOTA_Unit_Hero_Windrunner or wID == CDOTA_Unit_Hero_Shredder or wID == CDOTA_Unit_Hero_Centaur or wID == CDOTA_Unit_Hero_Earth_Spirit or wID == CDOTA_Unit_Hero_Ember_Spirit or wID == CDOTA_Unit_Hero_Storm_Spirit or who:FindItem("item_force_staff") or who.movespeed > 480) then
 			return true
 		else
 			return	false
