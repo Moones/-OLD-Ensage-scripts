bfdfbf10edaf918b4b4a1ab8e23590d8d9ae8a2e
Alois Pospíšil <moones@email.cz>
2014-09-16 16:35:35 +0200

    Update Templar_Assassin.lua

--- Scripts/Templar_Assassin.lua
+++ Scripts/Templar_Assassin.lua
@@ -144,9 +144,13 @@ function Main(tick)
 					if tick > sleep then
 						--move to mouse position
 						if SleepCheck("move") then
-							if not moveposition or GetDistance2D(client.mousePosition, moveposition) > 5 then 
-								me:Move(client.mousePosition)
-								moveposition = client.mousePosition
+							if victim and (GetDistance2D(client.mousePosition, victim) <= 10 or entityList:GetMouseover() == victim) then
+								me:Move(victim.position)
+							else
+								if not moveposition or GetDistance2D(client.mousePosition, moveposition) > 100 then 
+									me:Move(client.mousePosition)
+									moveposition = client.mousePosition
+								end
 							end
 							sleep = tick + 30 + client.latency
 						end
