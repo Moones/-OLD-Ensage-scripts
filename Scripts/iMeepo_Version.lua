3d34aa011d54fddeed7bf2bb68eccd14855943e2
Moones <moones@email.cz>
2015-04-30 23:21:18 +0200

    Update iMeepo_Version.lua

--- Scripts/iMeepo_Version.lua
+++ Scripts/iMeepo_Version.lua
@@ -1,3 +1,3 @@
-0.777
-
-Hey, this version should NOT CRASH anymore. I have only tested one real game and couple of lobbies tho. If you experience crash again you can report it to moones@email.cz
+0.8
+BETA
+This is fix for new Dota 6.84 update, I was not able to test more further so any errors please report to moones@email.cz thanks!
