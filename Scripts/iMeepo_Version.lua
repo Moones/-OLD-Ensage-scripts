5c4026e845e090e7c4908113087e15ed34eb2eec
Moones <moones@email.cz>
2015-05-01 00:57:31 +0200

    Update iMeepo_Version.lua

--- Scripts/iMeepo_Version.lua
+++ Scripts/iMeepo_Version.lua
@@ -1,3 +1,3 @@
 0.8
 BETA
-This is fix for new Dota 6.84 update, I was not able to test more further so any errors please report to moones@email.cz thanks!
+This is fix for new Dota 6.84 update, I was not able to test more further so any errors please report to moones@email.cz thanks! Redownload AbilityDamage library if you have Ethereal blade bug!
