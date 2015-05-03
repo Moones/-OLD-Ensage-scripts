6db1ab41cdad9ac4e25fb367a3ffd442395ba1ec
Moones <moones@email.cz>
2015-05-03 17:43:02 +0200

    Update iMeepo_Version.lua

--- Scripts/iMeepo_Version.lua
+++ Scripts/iMeepo_Version.lua
@@ -1,3 +1,3 @@
-0.8
-ALPHA
-Dont forget to download Utils from my repository to get the script working correctly. OrbWalking and Blocking enemy is broken due to Dota update I am sorry.
+0.81
+
+OrbWalking and Blocking enemy now works again!
