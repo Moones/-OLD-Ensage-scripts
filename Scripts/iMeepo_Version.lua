b9ceb620e9b680ff9e0e9853ac847c6bd67e9545
Moones <moones@email.cz>
2015-05-06 16:04:22 +0200

    Update iMeepo_Version.lua

--- Scripts/iMeepo_Version.lua
+++ Scripts/iMeepo_Version.lua
@@ -1,3 +1,3 @@
-0.813
-
-Had to do workaround for broken alive attribute. Chaining Earthbind broken currently, need to wait for Zynox again. Dont forget to add Rulfy\ensage-wip repo back.
+0.815
+BETA
+Please report errors/bugs to moones@email.cz
