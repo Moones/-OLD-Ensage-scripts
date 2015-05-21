8305046d3bdb11f53e709c6a93a1c998a1d882a8
Moones <moones@email.cz>
2015-05-21 21:57:50 +0200

    Update MoonesComboScript_Version.lua

--- Scripts/MoonesComboScript_Version.lua
+++ Scripts/MoonesComboScript_Version.lua
@@ -1,3 +1,3 @@
-0.132
+0.135
 
-Did performance rework, let me know if it is better or not. Please send me suggestions/errors/bugs to moones@email.cz
+Please send me suggestions/errors/bugs to moones@email.cz
